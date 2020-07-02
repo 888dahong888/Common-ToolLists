9924957837c164ef00d1bc7a5bf2c6a6efc6f3ae
LDH <909480900@qq.com>
2020-07-02 15:17:39 +0800

    th

---
+++ OP/Thread线程操作/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Thread线程操作
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Thread(Go).Start();
+            Go();
+
+            Thread s = new Thread(WriteY);
+            s.Start();
+            while (true)
+            {
+                Console.Write("x");
+            }
+            Console.Read();
+        }
+        private static void WriteY()
+        {
+            while (true)
+            {
+                Console.Write("y");
+            }
+        }
+
+        private static void Go()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
