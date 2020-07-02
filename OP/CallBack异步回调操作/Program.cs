dad747eb8f3565b06620e2b8cef5c0739bb01f46
LDH <909480900@qq.com>
2020-07-02 15:13:21 +0800

    cbas

---
+++ OP/CallBack异步回调操作/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CallBack异步回调操作
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AsyncCallback callback = new AsyncCallback(Test);
+
+            for (int i = 0; i < 10; i++)
+            {
+                //Thread.Sleep(1000);
+                Console.WriteLine($"主线程{i}");
+            }
+            Console.Read();
+        }
+
+        public static void Test(IAsyncResult result)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine($"委托线程{i}");
+            }
+        }
+    }
+}
